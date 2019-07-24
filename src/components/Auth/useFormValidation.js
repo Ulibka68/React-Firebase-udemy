cb80e110d972c72533d25945db5319a4a9dc6285
Gayrat V <ggvlasov@gmail.com>
2019-07-24 14:46:56 +0300

    6. Developing a Reusable Form Hook

--- src/components/Auth/useFormValidation.js
+++ src/components/Auth/useFormValidation.js
@@ -1,5 +1,28 @@
 import React from "react";
 
-function useFormValidation() {}
+function useFormValidation(initialState) {
+  const [values, setValues] = React.useState(initialState);
+
+  function handleChange(event) {
+
+    // https://ru.reactjs.org/docs/events.html
+        // Если вы всё же хотите обратиться к полям события асинхронно, вам нужно вызвать event.persist() на событии.
+        // Тогда оно будет извлечено из пула, что позволит вашему коду удерживать ссылки на это событие.
+    //  В видео на 8:10 раздел3 №8 объяснено
+
+    event.persist();
+    setValues(previousValues => ({
+      ...previousValues,
+      [event.target.name]: event.target.value
+    }));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log({ values });
+  }
+
+  return { handleSubmit, handleChange, values };
+}
 
 export default useFormValidation;
