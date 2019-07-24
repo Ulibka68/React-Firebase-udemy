40fd481b96746fdfe3481fad0e4895005a391a2b
Gayrat V <ggvlasov@gmail.com>
2019-07-24 14:11:27 +0300

    основной

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ margin: "0 auto", textAlign: "center" }}>
+        <h1>Программа запустилась</h1>
+      <img style={{ height: "200px" }} src="/logo.png" alt="Logo" />
+    </div>
+  );
+}
+
+export default App;
