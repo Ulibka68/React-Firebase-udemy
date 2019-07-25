8929b60111cccade3d57ae357954d4fb1d84817d
Gayrat V <ggvlasov@gmail.com>
2019-07-25 15:59:15 +0300

    9.Registering-and-Logging-in-Users-with Firebase-Auth

--- src/components/Auth/useFormValidation.js
+++ src/components/Auth/useFormValidation.js
@@ -1,18 +1,15 @@
 import React from "react";
 
-function useFormValidation(initialState, validate) {
+function useFormValidation(initialState, validate, authenticate) {
   const [values, setValues] = React.useState(initialState);
   const [errors, setErrors] = React.useState({});
   const [isSubmitting, setSubmitting] = React.useState(false);
 
-  // видео 9 5:02
-  // когда errors был обновлен мы смотрим на изменение объекта by running the use effect
-  // функция (callback ) запускается тогда, когда errors (второй параметр) изменен
   React.useEffect(() => {
     if (isSubmitting) {
       const noErrors = Object.keys(errors).length === 0;
       if (noErrors) {
-        console.log("authenticated", values);
+        authenticate();
         setSubmitting(false);
       } else {
         setSubmitting(false);
@@ -21,12 +18,6 @@ function useFormValidation(initialState, validate) {
   }, [errors]);
 
   function handleChange(event) {
-
-    // https://ru.reactjs.org/docs/events.html
-        // Если вы всё же хотите обратиться к полям события асинхронно, вам нужно вызвать event.persist() на событии.
-        // Тогда оно будет извлечено из пула, что позволит вашему коду удерживать ссылки на это событие.
-    //  В видео на 8:10 раздел3 №8 объяснено
-
     event.persist();
     setValues(previousValues => ({
       ...previousValues,
@@ -44,7 +35,6 @@ function useFormValidation(initialState, validate) {
     const validationErrors = validate(values);
     setErrors(validationErrors);
     setSubmitting(true);
-    console.log({ values });
   }
 
   return {
