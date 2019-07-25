3fe06c732ce294864ed6376d3157a3c3c9c59074
Gayrat V <ggvlasov@gmail.com>
2019-07-25 14:59:53 +0300

    7. Adding Validation to useFormValidation

--- src/components/Auth/useFormValidation.js
+++ src/components/Auth/useFormValidation.js
@@ -1,15 +1,26 @@
 import React from "react";
 
-function useFormValidation(initialState) {
+function useFormValidation(initialState, validate) {
   const [values, setValues] = React.useState(initialState);
+  const [errors, setErrors] = React.useState({});
+  const [isSubmitting, setSubmitting] = React.useState(false);
+
+  // видео 9 5:02
+  // когда errors был обновлен мы смотрим на изменение объекта by running the use effect
+  // функция (callback ) запускается тогда, когда errors (второй параметр) изменен
+  React.useEffect(() => {
+    if (isSubmitting) {
+      const noErrors = Object.keys(errors).length === 0;
+      if (noErrors) {
+        console.log("authenticated", values);
+        setSubmitting(false);
+      } else {
+        setSubmitting(false);
+      }
+    }
+  }, [errors]);
 
   function handleChange(event) {
-
-    // https://ru.reactjs.org/docs/events.html
-        // Если вы всё же хотите обратиться к полям события асинхронно, вам нужно вызвать event.persist() на событии.
-        // Тогда оно будет извлечено из пула, что позволит вашему коду удерживать ссылки на это событие.
-    //  В видео на 8:10 раздел3 №8 объяснено
-
     event.persist();
     setValues(previousValues => ({
       ...previousValues,
@@ -17,12 +28,27 @@ function useFormValidation(initialState) {
     }));
   }
 
+  function handleBlur() {
+    const validationErrors = validate(values);
+    setErrors(validationErrors);
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
+    const validationErrors = validate(values);
+    setErrors(validationErrors);
+    setSubmitting(true);
     console.log({ values });
   }
 
-  return { handleSubmit, handleChange, values };
+  return {
+    handleSubmit,
+    handleBlur,
+    handleChange,
+    values,
+    errors,
+    isSubmitting
+  };
 }
 
 export default useFormValidation;
