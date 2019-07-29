02e324ffc27d5eb81bfd69ec9dbcfe350a594a24
Gayrat V <ggvlasov@gmail.com>
2019-07-29 10:12:39 +0300

    24. Dynamically Ordering Links, Preparing for Pagination

--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -6,13 +6,21 @@ function LinkList(props) {
   const { firebase } = React.useContext(FirebaseContext);
   const [links, setLinks] = React.useState([]);
   const isNewPage = props.location.pathname.includes("new");
+  const isTopPage = props.location.pathname.includes("top");
 
   React.useEffect(() => {
-    getLinks();
-  }, []);
+    const unsubscribe = getLinks();
+    return () => unsubscribe();
+  }, [isTopPage]);
 
   function getLinks() {
-    firebase.db
+    if (isTopPage) {
+      return firebase.db
+        .collection("links")
+        .orderBy("voteCount", "desc")
+        .onSnapshot(handleSnapshot);
+    }
+    return firebase.db
       .collection("links")
       .orderBy("created", "desc")
       .onSnapshot(handleSnapshot);
@@ -25,19 +33,9 @@ function LinkList(props) {
     setLinks(links);
   }
 
-  function renderLinks() {
-    if (isNewPage) {
-      return links;
-    }
-    const topLinks = links
-      .slice()
-      .sort((l1, l2) => l2.votes.length - l1.votes.length);
-    return topLinks;
-  }
-
   return (
     <div>
-      {renderLinks().map((link, index) => (
+      {links.map((link, index) => (
         <LinkItem
           key={link.id}
           showCount={true}
