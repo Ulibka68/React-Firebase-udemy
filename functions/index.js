15b48190007d43ab81e8180ff948e38b1ad55efc
Gayrat V <ggvlasov@gmail.com>
2019-08-06 15:29:05 +0300

    26. Firebase Function for Offset Query с окончательными правками

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,21 @@
 const functions = require("firebase-functions");
 const LINKS_PER_PAGE = 5;
+// const {LINKS_PER_PAGE} = require('../src/utils');
 
 const admin = require("firebase-admin");
 admin.initializeApp({
   credential: admin.credential.applicationDefault(),
-  databaseURL: "https://hooks-news-app.firebaseio.com"
+  databaseURL: "https://catch-of-the-day-gayrat.firebaseio.com"
 });
 const db = admin.firestore();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/
 //
+// Вызов ../linksPagination?offset=20
+
+// Запрос
+// https://us-central1-catch-of-the-day-gayrat.cloudfunctions.net/linksPagination?offset=1
+
 exports.linksPagination = functions.https.onRequest((request, response) => {
   response.set("Access-Control-Allow-Origin", "*");
   let linksRef = db.collection("links");
